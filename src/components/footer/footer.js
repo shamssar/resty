f5d301237204204feba62bf9db8a24288c4963b2
shamssar <nayefshams3@gmail.com>
2022-08-09 17:08:12 +0300

    done

---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import './footer.scss'
+const Footer = () => { 
+    return (
+        <>
+        <footer>
+            <p>&copy; 2022 shams Alsarireh</p>
+        </footer>
+        </>
+    )
+}
+
+export default Footer;
---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import './header.scss';
+const Header = function () {
+    return (
+      <>
+        <header>
+        <h1>RESTy</h1>
+      
+
+      </header>
+      </>
+    );
+  };
+  
+  export default Header;
