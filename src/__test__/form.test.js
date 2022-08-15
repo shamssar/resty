8a9b72cd625f9d5e1176641782afedb474bb24c9
shamssar <nayefshams3@gmail.com>
2022-08-15 15:07:31 +0300

    add effect hook

--- src/__test__/form.test.js
+++ src/__test__/form.test.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { render, waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import App from '../app.js';
+import axios from 'axios';
+import Results from '../components/results/results';
+// test('testing if the default state for method is GET ', async () => {
+//     render(<App />);
+//     const GET = await waitFor(() => screen.getByTestId("GET"));
+//     expect(GET).toHaveTextContent('GET');
+// }); 
 
-test('testing if the default state for method is GET ', async () => {
-    render(<App />);
-    const GET = await waitFor(() => screen.getByTestId("GET"));
-    expect(GET).toHaveTextContent('GET');
-}); 
+
+
+
+it('Upon submitting the form will result in data being rendered in the output area', async () => {
+  const data = await axios({
+    method: 'GET',
+    url: 'https://pokeapi.co/api/v2/pokemon/ditto',
+  });
+
+  render(<Results data={data} />);
+  expect(data).toBeTruthy();
+});
+
+
+describe('testing the results component', () => {
+  it('should render data.data', () => {
+    let dataProp = { data: 'test' };
+
+    render(<Results data={dataProp} />);
+    let displayText = screen.getByTestId('data');
+    expect(displayText).toHaveTextContent(dataProp.data);
+  });
+});
